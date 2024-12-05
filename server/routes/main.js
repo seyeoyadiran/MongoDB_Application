84ffc72dc74f0a981fc74eab20d16127b4d29903
seyeoyadiran <seyeoyadiran@gmail.com>
2024-12-04 23:27:35 -0500

    Adding more stuff

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,20 +1,70 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../models/Post');
 
 
-//Routes
+/*
+Get / Home 
+*/
 
-router.get('', (req, res) => {
-    const locals = {
-        title: "Oluwaseye's Blog" , 
-        description: "Simple Blog Creatd with NodeJs, Express, and Mongodb"
+router.get('', async (req, res) => {
+    try {
+        const locals = {
+            title: "Oluwaseye's Blog" , 
+            description: "Simple Blog Creatd with NodeJs, Express, and Mongodb"
+        }
+
+        let perPage = 10;
+        let page = req.query.page || 1;
+
+        const data = await Post.aggregate([ { $sort: { createdAt: -1 } }])
+        .skip(perPage * page - perPage)
+        .limit(perPage)
+        .exec();
+
+        
+        const count = await Post.countDocuments();
+        const nextPage = parseInt(page) + 1;
+        const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+        res.render('index', {
+            locals,
+             data,
+             current: page, 
+             nextPage: hasNextPage ? nextPage : null
+            });
+
+    } catch (error) {
+        console.log(error);
     }
-
-    res.render('index', {locals});
 });
+
+
+// router.get('', async (req, res) => {
+//     const locals = {
+//         title: "Oluwaseye's Blog" , 
+//         description: "Simple Blog Creatd with NodeJs, Express, and Mongodb"
+//     }
+
+//     try {
+//         const data = await Post.find();
+//         res.render('index', {locals, data});
+//     } catch (error) {
+//         console.log(error);
+//     }
+// });
+
+
+
+
+
 
 router.get('/about', (req, res) => {
     res.render('about')
+})
+
+router.get('/contact', (req, res) => {
+    res.render('contact')
 })
 
 module.exports = router; 
